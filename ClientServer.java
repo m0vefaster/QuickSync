7d30dada19485a9f28205efee01b35f945cbfe92
vish1562 <vishal1562@gmail.com>
2015-02-07 03:57:36 -0800

    Working With Single Directory

--- ClientServer.java
+++ ClientServer.java
@@ -4,24 +4,32 @@ import java.net.*;
 
 public class ClientServer{
     public static void main(String[] args){
-        Thread client1 = new Thread(new Client(args[0], args[1], args[2]));
-        Thread client2 = new Thread(new Client(args[3], args[1], args[4]));
 
-        client1.start();
-        client2.start();
-        ServerSocket ss = null;
-        while(true){
-            try {
+	    
+       ServerSocket ss = null;
+        
+	while(true)
+	{
+            try 
+	    {
 	    	ss = new ServerSocket(Integer.parseInt(args[1]));
                 Socket s = ss.accept();
                 Thread server = new Thread(new Server(ss, s));
                 System.out.println("ClientServer:Created Thread for " +s. getRemoteSocketAddress());
 		server.start();
-            } catch (Exception e) {
-        	try{
+                Thread client1 = new Thread(new Client(args[0], args[1], args[2]));
+		client1.start();
+            }
+	    catch (Exception e) 
+	    {
+        	try
+		{
 			Thread.sleep(100);   
-		}catch(Exception e1){}
-	}
-        }
+		}
+		catch(Exception e1){}
+	    }
+        } 
+
+        
     }
 }
