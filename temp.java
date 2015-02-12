b08ca9e555c4aa38113c7eba2239de2ddc670ea5
vish1562 <vishal1562@gmail.com>
2015-02-12 10:58:06 -0800

    Changed PeerFileList and temp

--- temp.java
+++ temp.java
@@ -1,13 +1,32 @@
-/*
+
 void keepChecking()
 {
 	ListOfFiles lof = new ListOfFiles();
 
 	while(true)
 	{
-	    Array<String> localFiles = lof.getList();
         ArrayList<ArrayList<String>> llof = peerList.getPeerListOfFiles();
-
+        PeerFileList pfl = new PeerFileList();
+		llof.add(self.getListOfFiles());
+        HashMap<String,ArrayList<String>> hm = pfl.getListOFPeersWithFiles(llof);		
+	    //Send HashMap 	
 	}
 }
-*/
+
+
+//To get when Controller
+HashMap<String,ArrayList<String>> getHashMapOfFile(PeerNode self)
+{
+        ArrayList<ArrayList<String>> llof = peerList.getPeerListOfFiles();
+        PeerFileList pfl = new PeerFileList();
+        llof.add(self.getListOfFiles());
+        HashMap<String,ArrayList<String>> hm = pfl.getListOFPeersWithFiles(llof); 
+        return hm;
+}
+
+
+
+//For Peer Node
+
+
+
