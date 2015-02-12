85e22d95ce1c9671ab706a208dd4c90aa27a3d8e
vish1562 <vishal1562@gmail.com>
2015-02-12 11:29:11 -0800

    Will have to change PeerFileList

--- ListOfFiles.java
+++ ListOfFiles.java
@@ -4,7 +4,7 @@ import java.lang.*;
 
 class ListOfFiles
 {
-    static String homeDir = System.getProperty("user.home");
+      static String homeDir = System.getProperty("user.home");
       static String folder = "QuickSync";
       static String path = homeDir + "/" + folder ;
 
