28609f24711cac7291e112f4219348a0a0afed92
agarwalpallavi <Kaushalya_1>
2015-02-12 21:47:04 -0800

    json added to tcpServer

--- TcpServer.java
+++ TcpServer.java
@@ -9,6 +9,10 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class TcpServer implements Runnable
 {
@@ -29,24 +33,30 @@ public class TcpServer implements Runnable
             while(true){
                 InputStream inFromServer = s.getInputStream();
                 DataInputStream in = new DataInputStream(inFromServer);
-                String line = null;
+                String line,str = null;
                 line = in.readUTF();
-                if(line.compareTo("***EOF***") == 0){
-                    System.out.println("Server: EOF received");
-                    break;
-                }else{
-                    System.out.println("Server: File request received for " + line);
+                JSONObject obj = (JSONObject)(JSONManager.convertStringToJSON(line));
+                if(obj.get("type").equals("Control"))
+                {
+                  str = (String)obj.get("value");  
+                  if(str.compareTo("***EOF***") == 0){
+                      System.out.println("Server: EOF received");
+                      break;
+                  }else{
+                      System.out.println("Server: File request received for " + line);
+                  }
                 }
-
                 /* Send the corresponding file */
-                myFile = new File(line);
+                
+                myFile = new File(str);
+                JSONObject toSend = JSONManager.getJSON(myFile);
                 byte[] mybytearray = new byte[(int) myFile.length()];
-                BufferedInputStream bis = new BufferedInputStream(new FileInputStream(myFile));
-                bis.read(mybytearray, 0, mybytearray.length);
+                //BufferedInputStream bis = new BufferedInputStream(new FileInputStream(myFile));
+                //bis.read(mybytearray, 0, mybytearray.length);
                 OutputStream os = s.getOutputStream();
-                os.write(mybytearray, 0, mybytearray.length);
+                os.write(toSend.toString().getBytes(), 0, toSend.toString().length());
                 os.flush(); 
-                System.out.println("Server: Sent file " + line);
+                System.out.println("Server: Sent file " + str);
             }
             s.close();
             System.out.println("Server: closing socket "+s.toString());
