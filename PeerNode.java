e672e951268bd41dc122643642ca87dc9e248c05
agarwalpallavi <pallaviagarwal90@gmail.com>
2015-02-28 18:21:58 -0800

    PeerNode socket addition

--- PeerNode.java
+++ PeerNode.java
@@ -4,8 +4,10 @@ import java.net.*;
 
 public class PeerNode implements Serializable{
     private String peerId;
+    private Socket socket;
     private int peerWeight;
     private ListOfFiles lof;
+    private boolean isCloud;
     private HashMap<String, ArrayList<String>> hMap;
     String ipAddress ;
 
@@ -16,6 +18,7 @@ public class PeerNode implements Serializable{
         lof = new ListOfFiles();
         hMap = new HashMap<String, ArrayList<String>>();
         this.ipAddress=ipAddress;
+        this.socket = null;
     }
     
     PeerNode(String peerId, String ipAddress, int peerWeight){
@@ -24,6 +27,35 @@ public class PeerNode implements Serializable{
         lof = new ListOfFiles();
         hMap = new HashMap<String, ArrayList<String>>();
         this.ipAddress=ipAddress;
+        this.socket = null;
+    }
+    
+    PeerNode(String ipAddress, Socket socket){
+        lof = new ListOfFiles();
+        hMap = new HashMap<String, ArrayList<String>>();
+        this.ipAddress=ipAddress;
+        this.socket = socket;
+    }
+    
+    public void setSocket(Socket socket)
+    {
+        this.socket = socket;
+    }
+
+    public void setIsCloud(boolean isCloud)
+    {
+        this.isCloud = isCloud;
+    }
+
+    
+    public Socket getSocket()
+    {
+        return socket;
+    }
+
+    public boolean getIsCloud()
+    {
+        return isCloud;
     }
     
     public String getId()
